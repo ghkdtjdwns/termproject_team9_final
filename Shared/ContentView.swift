231b3274ca0a7aa23b242d195a03973003129793
ymko <kymandoo0427@yonsei.ac.kr>
2023-06-06 13:22:32 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 고영민 on 2023/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/termproject_teampApp.swift
@@ -0,0 +1,17 @@
+//
+//  termproject_teampApp.swift
+//  Shared
+//
+//  Created by 고영민 on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct termproject_teampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
